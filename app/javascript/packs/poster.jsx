bacdacf16355a5f2f784b0ce1903a9a34ed75a70
komalswami <komalsswami@gmail.com>
2022-08-04 22:47:08 +0530

    init

---
+++ app/javascript/packs/poster.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './poster.css'
+function poster() {
+  return (
+    <div className='poster'>
+        <img className="home__image"
+        src='https://www.merokirana.com/archive/KiranaOffer/9ab5f55e5efa4faaa1b9fe4c5c2ee0bf.gif'
+        alt='img_poster'
+        />
+    </div>
+  )
+}
+
+export default poster
